4580cc05e4d5d0ac788991af64eed7b1bb587662
ITR <ITR@live.no>
2022-08-04 15:21:48 +0200

    Initial Commit

---
+++ MainClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MelonLoader;
+using VrcTracer;
+
+[assembly: MelonInfo(typeof(MainClass), "ITR's Tweaks", "0.1.0", "ITR")]
+[assembly: MelonGame("Flanne", "MinutesTillDawn")]
+namespace VrcTracer
+{
+    public class MainClass : MelonMod
+    {
+        public static Action<string> Msg { get; private set; }
+        public static Action<string> Warning { get; private set; }
+        public static Action<string> Error { get; private set; }
+
+        public override void OnApplicationStart()
+        {
+            Msg = LoggerInstance.Msg;
+            Warning = LoggerInstance.Warning;
+            Error = LoggerInstance.Error;
+        }
+    }
+}
