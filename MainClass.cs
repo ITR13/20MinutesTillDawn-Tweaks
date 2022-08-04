837f717547a3e11dcfc48f38e97b7df86cf75858
ITR <ITR@live.no>
2022-08-05 00:59:39 +0200

    Made volume increase in other increments than 25

--- MainClass.cs
+++ MainClass.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 using MelonLoader;
-using VrcTracer;
+using ItrsTweaks;
 
 [assembly: MelonInfo(typeof(MainClass), "ITR's Tweaks", "0.1.0", "ITR")]
 [assembly: MelonGame("Flanne", "MinutesTillDawn")]
-namespace VrcTracer
+namespace ItrsTweaks
 {
     public class MainClass : MelonMod
     {
@@ -12,11 +12,18 @@ namespace VrcTracer
         public static Action<string> Warning { get; private set; }
         public static Action<string> Error { get; private set; }
 
+        public static MelonPreferences_Entry<int> _volumePercent;
+
+        public static int VolumePercent => _volumePercent.Value;
+
         public override void OnApplicationStart()
         {
             Msg = LoggerInstance.Msg;
             Warning = LoggerInstance.Warning;
             Error = LoggerInstance.Error;
+
+            var category = MelonPreferences.CreateCategory("ItrsTweaks");
+            _volumePercent = category.CreateEntry("VolumeStep", 10, "Volume Step (Percent)", "How many percent the volume slider steps every time you click it");
         }
     }
 }
---
+++ OptionsSetterPatch.cs
@@ -0,0 +1,24 @@
+﻿using flanne;
+using flanne.UI;
+using HarmonyLib;
+
+namespace ItrsTweaks
+{
+    [HarmonyPatch(typeof(OptionsSetter), nameof(OptionsSetter.OnClickSFXVolume))]
+    public class SfxVolumePatch
+    {
+        public static void Prefix(AudioManager ___AM)
+        {
+            ___AM.SFXVolume = ___AM.SFXVolume - 0.25f + MainClass.VolumePercent / 100f;
+        }
+    }
+
+    [HarmonyPatch(typeof(OptionsSetter), nameof(OptionsSetter.OnClickBGMVolume))]
+    public class BgmVolumePatch
+    {
+        public static void Prefix(AudioManager ___AM)
+        {
+            ___AM.MusicVolume = ___AM.MusicVolume - 0.25f + MainClass.VolumePercent / 100f;
+        }
+    }
+}
