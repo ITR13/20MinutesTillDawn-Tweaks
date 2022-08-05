bbb194f783277935154f8a56a2dc499fc261dab5
ITR <ITR@live.no>
2022-08-05 21:49:03 +0200

    Added timescale cheat

---
+++ LargerThan0ValueValidator.cs
@@ -0,0 +1,36 @@
+﻿using MelonLoader.Preferences;
+
+namespace ItrsTweaks
+{
+    public class LargerThan0ValueValidator : ValueValidator
+    {
+        public override object EnsureValid(object value)
+        {
+            switch (value)
+            {
+                case float f:
+                    if (f < 0.01f) return 0.01f;
+                    break;
+                case int i:
+                    if (i <= 0) return 1;
+                    break;
+            }
+            return value;
+        }
+
+        public override bool IsValid(object value)
+        {
+            switch (value)
+            {
+                case float f:
+                    if (f < 0.01f) return false;
+                    break;
+                case int i:
+                    if (i <= 0) return false;
+                    break;
+            }
+
+            return true;
+        }
+    }
+}
--- MainClass.cs
+++ MainClass.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using MelonLoader;
 using ItrsTweaks;
+using UnityEngine;
+using MelonLoader.Preferences;
 
 [assembly: MelonInfo(typeof(MainClass), "ITR's Tweaks", "0.1.0", "ITR")]
 [assembly: MelonGame("Flanne", "MinutesTillDawn")]
@@ -18,6 +20,7 @@ namespace ItrsTweaks
         private static MelonPreferences_Entry<bool> _noAttack;
         private static MelonPreferences_Entry<bool> _fixRandomShot;
         private static MelonPreferences_Entry<bool> _randomShotsAffectOnShoot;
+        private static MelonPreferences_Entry<float> _timeScale;
 
         public static int VolumePercent => _volumePercent.Value;
         public static bool HoldToActivateSkill => _holdToActivateSkill.Value;
@@ -32,16 +35,29 @@ namespace ItrsTweaks
             Error = LoggerInstance.Error;
             Eerror = LoggerInstance.Error;
 
-            var category = MelonPreferences.CreateCategory("ItrsTweaks");
+            var category = MelonPreferences.CreateCategory("ITR'sTweaks");
 
-            _volumePercent = category.CreateEntry("VolumeStep", 10, "Volume Step (Percent)", "How many percent the volume slider steps every time you click it");
+            _volumePercent = category.CreateEntry("VolumeStep", 10, "Volume Step (Percent)", "How many percent the volume slider steps every time you click it", false, false, new LargerThan0ValueValidator());
             _holdToActivateSkill = category.CreateEntry("HoldToActivateSkill", true, "Automatic Skill", "Allows you to hold right click to automatically fire your skill");
             _fixRandomShot = category.CreateEntry("FixRandomShot", true, "Fix Random Shots", "Makes Abby's passive less biased");
 
-            var challengesCategory = MelonPreferences.CreateCategory("ItrsChallenges");
+            var cheatsCategory = MelonPreferences.CreateCategory("ITR'sCheats");
+            _timeScale = cheatsCategory.CreateEntry("TimeScale", 1f, "Time scale", "Overwrites how fast the game is when not paused", false, false, new LargerThan0ValueValidator());
+
+            var challengesCategory = MelonPreferences.CreateCategory("ITR'sChallenges");
 
             _noAttack = challengesCategory.CreateEntry("NoAttack", false, "Disable Main Attack", "Prevents you from attacking and reloading with your left mouse button");
             _randomShotsAffectOnShoot = challengesCategory.CreateEntry("AbbyExtraChallenge", false, "Abby Extra Challenge", "Makes Abby's passive also affect powerups that fire every nth shot");
+        }
+
+        public override void OnLateUpdate()
+        {
+            var oldTimescale = Time.timeScale;
+            var timescale = _timeScale.Value;
+            if (oldTimescale != 0 && oldTimescale != timescale)
+            {
+                Time.timeScale = timescale;
+            }
         }
     }
 }
